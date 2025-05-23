4de6bf40ab0ef4174c4b938b37b3580788c08a9a
conbocuoila <phucp9698@gmail.com>
2025-05-23 08:00:57 +0700

    update

--- src/pages/authen/register/index.jsx
+++ src/pages/authen/register/index.jsx
@@ -1,31 +1,30 @@
-import './index.scss';
-import { Form, Input, Button, Typography } from 'antd';
+import "./index.scss";
+import { Form, Input, Button, Typography } from "antd";
 
-import logoBee from '../../../assets/images/1740063267602.gif';
-import { useNavigate } from 'react-router-dom';
-import { useForm } from 'antd/es/form/Form';
-import api from '../../../config/api';
-import { toast } from 'react-toastify';
-import { useState } from 'react';
+import logoBee from "../../../assets/images/1740063267602.gif";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "antd/es/form/Form";
+import api from "../../../config/api";
+import { toast } from "react-toastify";
+import { useState } from "react";
 
 const { Title, Text } = Typography;
 
 const RegisterPage = () => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const [form] = useForm();
   const onFinish = async (values) => {
-    setLoading(true)
+    setLoading(true);
     try {
-      await api.post('register', values)
-      toast.success("Register successfully!")
-      navigate("/login-page")
+      await api.post("register", values);
+      toast.success("Register successfully!");
+      navigate("/login-page");
     } catch (error) {
-      toast.error(error.response.data.message)
+      toast.error(error.response.data.message);
     }
-    setLoading(false)
+    setLoading(false);
   };
-
 
   return (
     <div className="register-page">
@@ -34,9 +33,14 @@ const RegisterPage = () => {
           <img src={logoBee} alt="Bee" />
         </div>
 
-        <Title level={2} className="title">Ready to<br />take breaks?</Title>
+        <Title level={2} className="title">
+          Ready to
+          <br />
+          take breaks?
+        </Title>
 
-        <Form form={form}
+        <Form
+          form={form}
           name="register-form"
           layout="vertical"
           className="register-form"
@@ -44,27 +48,32 @@ const RegisterPage = () => {
         >
           <Form.Item
             name="username"
-            rules={[{ required: true, message: 'Please enter your username' }]}
+            rules={[{ required: true, message: "Please enter your username" }]}
           >
             <Input placeholder="Your Username" className="input" />
           </Form.Item>
 
-
-
           <Form.Item
             name="password"
-            rules={[{ required: true, message: 'Please enter your password' }]}
+            rules={[
+              { required: true, message: "Please enter your password" },
+              { min: 8, message: "Password must be at least 8 characters" },
+            ]}
           >
             <Input.Password placeholder="Password" className="input" />
           </Form.Item>
-
-
 
           <Text className="signin-text" onClick={() => navigate("/login-page")}>
             Already have an account? <span>Sign In</span>
           </Text>
 
-          <Button loading={loading} onClick={() => form.submit()} type="primary" className="submit-btn" block>
+          <Button
+            loading={loading}
+            onClick={() => form.submit()}
+            type="primary"
+            className="submit-btn"
+            block
+          >
             I’m Ready!
           </Button>
         </Form>
